eb73de20c59cdb35ae6588e79fccc0762fbb5b71
joaolopeslima <contato.jantonio@gmail.com>
2022-03-24 18:10:46 -0300

    definindo limites nos ranges +desenvolvendo js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function checagem() {
     
     var selMed = document.getElementById('selMed');
 
-    var selHorario = document.getElementById('selHorario')
+    var selHorario = document.getElementById('selHorario');
 
     var checaMed = selMed.options[selMed.selectedIndex].value;
 
-    var checaHorario = selHorario.options[selHorario.selectedIndex].value
+    var checaHorario = selHorario.options[selHorario.selectedIndex].value;
 
     if (checaMed == 0 || checaHorario == 0){
         alert ("selecione todos os itens");
