4ef89151ce7fe2210167054e7a632a779d695444
joaolopeslima <contato.jantonio@gmail.com>
2022-03-25 19:31:09 -0300

    captura de dados + instalação do express

--- script.js
+++ script.js
@@ -16,4 +16,27 @@ function checagem() {
     }
 
  }
-    
+    function capturaDados(){
+
+        var textoNome = document.getElementById('inputNome').value
+        var textoSobrenome = document.getElementById('inputSobrenome').value
+        var textoPeso = document.getElementById('inputPeso').value
+        var textoAltura = document.getElementById('inputAltura').value
+        var textoPressao = document.getElementById('inputPressao').value
+        var textoSaturacao = document.getElementById('inputSaturacao').value
+        var textoCPF = document.getElementById('inputCPF').value
+        var textoNascimento = document.getElementById('inputNascimento').value
+
+        console.log("nome: " + textoNome)
+        console.log("Sobrenome: " + textoSobrenome)
+        console.log("Peso: " + textoPeso)
+        console.log("Altura: " + textoAltura)
+        console.log("Pressão: " + textoPressao)
+        console.log("Saturação: " + textoSaturacao)
+        console.log("CPF: " + textoCPF)
+        console.log("Data de Nascimento: " + textoNascimento)
+
+    }
+
+    /*dados:
+    Nome, Sobrenome, Peso, Altura, Pressão Arterial, Saturação, CPF, Data de Nascimento */ 
